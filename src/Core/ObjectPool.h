33f7583eea4618e2637d8eef17fb4c75b9f058f7
Nguyen Tran <merlinvn@gmail.com>
2017-05-26 03:30:49 +0700

    fix std::make_unique for C++11

--- src/Core/ObjectPool.h
+++ src/Core/ObjectPool.h
@@ -12,12 +12,12 @@
 #include <ostream>
 
 
-//namespace std {
-//    template<typename T, typename... Args>
-//    std::unique_ptr<T> make_unique(Args &&... args) {
-//        return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
-//    }
-//}
+namespace std {
+   template<typename T, typename... Args>
+   std::unique_ptr<T> make_unique(Args &&... args) {
+       return std::unique_ptr<T>(new T(std::forward<Args>(args)...));
+   }
+}
 
 class IPoolable {
     virtual void init() = 0;
